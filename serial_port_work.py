0f3333263a1525138c68aa089c033bb63ad7d412
Burov Nikita <burov.nm@yandex.ru>
2025-03-14 14:26:48 +0300

    Add cycle when you work with port

--- serial_port_work.py
+++ serial_port_work.py
@@ -12,25 +12,24 @@ def send_command_to_port(ser: serial.Serial, command):
     return
 
 def work_with_port(ser: serial.Serial):
-
-    print('------------------------------------------')
-    print(f'Начало работы с портом {ser}')
-    print('------------------------------------------')
-    print(f'Выберите формат действия \n1 - Считать данные \n2 - Отправить команду в порт \n 0 - Вернуться к Настройке порта')
-    print('------------------------------------------')
-    try:
-        command = int(input())
-        if command == 1:
-            print('Считанные данные:')
-            answer = read_all_data_from_port(ser)
-            print(f'Считанные данные: {answer}')
-        elif command == 2:
-            print('Отправка команды в порт')
-            port_command = input('Введите команду: ')
-            send_command_to_port(ser, port_command)
-        elif command == 0:
-            print('Возврат в меню настройки порта')
-            return
-    except Exception as e:
-        print(f"Ошибка открытия: {e}")
-    pass
+    while True:
+        print('------------------------------------------')
+        print(f'Начало работы с портом {ser}')
+        print('------------------------------------------')
+        print(f'Выберите формат действия \n1 - Считать данные \n2 - Отправить команду в порт \n 0 - Вернуться к Настройке порта')
+        print('------------------------------------------')
+        try:
+            command = int(input())
+            if command == 1:
+                print('Считанные данные:')
+                answer = read_all_data_from_port(ser)
+                print(f'Считанные данные: {answer}')
+            elif command == 2:
+                print('Отправка команды в порт')
+                port_command = input('Введите команду: ')
+                send_command_to_port(ser, port_command)
+            elif command == 0:
+                print('Возврат в меню настройки порта')
+                return
+        except Exception as e:
+            print(f"Ошибка открытия: {e}")
